fa99121fe3793daed6795516da483c50364d2f8a
Obscuria <obscurialithium@gmail.com>
2026-05-20 23:38:45 -0300

    Add contributors section

---
+++ src/data/contributors.ts
@@ -0,0 +1,28 @@
+export const TEAM_MEMBERS: string[] = [
+  'ObscuriaLithium', 
+  'Mr-Cakeman', 
+  'squarzyy'
+];
+
+export const COMMUNITY_MEMBERS: string[] = [
+  'F-708', 
+  'SiverDX', 
+  'foiledknight', 
+  'DarkEclipse86',
+  'Huacaii', 
+  'Cicopath', 
+  'PiggyDr', 
+  'Heimdallr-1', 
+  'Kasualix'
+];
+
+export const CONTRIBUTOR_REPOS: string[] = [
+  'ObscuriaLithium/aquamirae',
+  'ObscuriaLithium/obscure-tooltips',
+  'ObscuriaLithium/archogenum',
+  'ObscuriaLithium/accents',
+  'ObscuriaLithium/pillager-caravans',
+  'ObscuriaLithium/ars-elixirum',
+  'ObscuriaLithium/maestro',
+  'ObscuriaLithium/fragmentum',
+];
